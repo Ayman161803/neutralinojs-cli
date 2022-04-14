3ce0d957d60bb8fbf9cb459875b21016c80c0403
Ayman161803 <mohammedaymannawaz@gmail.com>
2022-04-15 01:52:34 +0530

    make the temporary directory a bit consistent with the other parts of the framework

--- src/modules/downloader.js
+++ src/modules/downloader.js
@@ -24,15 +24,15 @@ let getRepoNameFromTemplate = (template) => {
 
 let downloadBinariesFromRelease = () => {
     return new Promise((resolve, reject) => {
-        fs.mkdirSync('temp', { recursive: true });
-        const file = fs.createWriteStream('temp/binaries.zip');
+        fs.mkdirSync('.tmp', { recursive: true });
+        const file = fs.createWriteStream('.tmp/binaries.zip');
         utils.log('Downloading Neutralinojs binaries..');
         https.get(getBinaryDownloadUrl(), function (response) {
             response.pipe(file);
             response.on('end', () => {
                 utils.log('Extracting zip file..');
-                fs.createReadStream('temp/binaries.zip')
-                    .pipe(unzipper.Extract({ path: 'temp/' }))
+                fs.createReadStream('.tmp/binaries.zip')
+                    .pipe(unzipper.Extract({ path: '.tmp/' }))
                     .promise()
                         .then(() => resolve())
                         .catch((e) => reject(e));
@@ -43,8 +43,8 @@ let downloadBinariesFromRelease = () => {
 
 let downloadClientFromRelease = () => {
     return new Promise((resolve, reject) => {
-        fs.mkdirSync('temp', { recursive: true });
-        const file = fs.createWriteStream('temp/neutralino.js');
+        fs.mkdirSync('.tmp', { recursive: true });
+        const file = fs.createWriteStream('.tmp/neutralino.js');
         utils.log('Downloading the Neutralinojs client..');
         https.get(getClientDownloadUrl(), function (response) {
             response.pipe(file);
@@ -60,18 +60,18 @@ let downloadClientFromRelease = () => {
 module.exports.downloadTemplate = (template) => {
     return new Promise((resolve, reject) => {
         let templateUrl = constants.remote.templateUrl.replace('{template}', template);
-        fs.mkdirSync('temp', { recursive: true });
-        const file = fs.createWriteStream('temp/template.zip');
+        fs.mkdirSync('.tmp', { recursive: true });
+        const file = fs.createWriteStream('.tmp/template.zip');
         https.get(templateUrl, function (response) {
             response.pipe(file);
             response.on('end', () => {
                 utils.log('Extracting template zip file..');
-                fs.createReadStream('temp/template.zip')
-                    .pipe(unzipper.Extract({ path: 'temp/' }))
+                fs.createReadStream('.tmp/template.zip')
+                    .pipe(unzipper.Extract({ path: '.tmp/' }))
                     .promise()
                         .then(() => {
-                            fse.copySync(`temp/${getRepoNameFromTemplate(template)}-main`, '.');
-                            utils.clearCache('temp');
+                            fse.copySync(`.tmp/${getRepoNameFromTemplate(template)}-main`, '.');
+                            utils.clearCache('.tmp');
                             resolve();
                         })
                         .catch((e) => reject(e));
@@ -89,14 +89,14 @@ module.exports.downloadAndUpdateBinaries = async () => {
     for(let platform in constants.files.binaries) {
         for(let arch in constants.files.binaries[platform]) {
             let binaryFile = constants.files.binaries[platform][arch];
-            fse.copySync(`temp/${binaryFile}`, `bin/${binaryFile}`);
+            fse.copySync(`.tmp/${binaryFile}`, `bin/${binaryFile}`);
         }
     }
 
     for(let dependency of constants.files.dependencies) {
-        fse.copySync(`temp/${dependency}`,`bin/${dependency}`);
+        fse.copySync(`.tmp/${dependency}`,`bin/${dependency}`);
     }
-    utils.clearCache('temp');
+    utils.clearCache('.tmp');
 }
 
 module.exports.downloadAndUpdateClient = async () => {
@@ -104,7 +104,7 @@ module.exports.downloadAndUpdateClient = async () => {
     const clientLibrary = utils.trimPath(configObj.cli.clientLibrary);
     await downloadClientFromRelease();
     utils.log('Finalizing and cleaning temp. files...');
-    fse.copySync(`temp/${constants.files.clientLibrary}`, `./${clientLibrary}`);
-    utils.clearCache('temp');
+    fse.copySync(`.tmp/${constants.files.clientLibrary}`, `./${clientLibrary}`);
+    utils.clearCache('.tmp');
 }
 
